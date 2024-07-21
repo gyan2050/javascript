e080f93b24943bd4292fc46470bc05fc037b2d49
mrbean <117147158+gyan2050@users.noreply.github.com>
2024-07-21 16:26:48 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,6 +1,7 @@
 const accountId = 1730
 let accountEmail = "pgyan1730@gmail.com"
-// var accountpassword = "12345" 
+// var accountpassword = "12345"
+var x=5 
 accountCity = "Jaipur"
 
 console.log(accountId);
