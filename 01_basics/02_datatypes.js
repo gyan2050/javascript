fa328104fd2efc358dd835022c4b5fe044a13962
mrbean <117147158+gyan2050@users.noreply.github.com>
2024-07-21 16:46:44 +0000

    datatypes

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,2 +1,17 @@
 "use strict";
 console.log(3+3)
+
+// Number 2^53
+// BigInt 
+// Boolean --true/false
+// String -- "njn"
+// null --standalone value
+// undefined 
+// symbol --uniqueness
+
+console.log(typeof null);
+console.log(typeof 2);
+console.log(typeof "hu");
+console.log(typeof true);
+
+
