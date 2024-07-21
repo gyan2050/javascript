0d7dddcde05b5c6c8896931e09ac86d20e990f9e
mrbean <117147158+gyan2050@users.noreply.github.com>
2024-07-21 16:19:35 +0000

    dev container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountId = 1730
+let accountEmail = "pgyan1730@gmail.com"
+// var accountpassword = "12345" 
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountpassword,accountCity]);
